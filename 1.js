42803df18e67be2795f87f1f77864fc9ce27126a
Duong-Van-Bao <duongbao9109@gmail.com>
2022-01-20 01:04:15 +0700

    add bt scss cuoi khoa

---
+++ 1.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.cacngonngu').slideUp();
+    $('.top-ngonngu b').click(function(event) {
+        $(this).next().slideToggle(300);
+    });
+    //kich hoat wow
+    new WOW().init();
+})
